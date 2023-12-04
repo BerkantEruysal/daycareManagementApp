a97b8fe0454631e6922c1bb44163ebd154e2dd0f
ArwensChild <75691062+BerkantEruysal@users.noreply.github.com>
2023-12-05 01:19:49 +0300

    Add create, list, delete functions

--- KresYonetimi.c
+++ KresYonetimi.c
@@ -1,5 +1,400 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
+typedef struct Student {
+    char name[50];
+    int age;
+    struct Student* next;
+} Student;
+
+typedef struct Class {
+    char className[30];
+    Student* studentList;
+    char toyList[5][30];
+    struct Class* next;
+} Class;
+
+// Fonksiyon prototipleri
+void createClass(Class** classList);
+void addStudent(Class* classList);
+void listClasses(Class* classList);
+void listStudents(Class* classList);
+void removeClass(Class** classList);
+void removeStudent(Class* classList);
+void addToy(Class* classList);
+void removeToy(Class* classList);
+void listToys(Class* classList);
+
+int main() {
+    Class* classList = NULL;
+    int choice;
+
+    do {
+        printf("\n--- Ana Menu ---\n");
+        printf("1- Yeni bir sinif olustur\n");
+        printf("2- Yeni bir ogrenci ekle\n");
+        printf("3- Siniflari listele\n");
+        printf("4- Ogrencileri listele\n");
+        printf("5- Sinif sil\n");
+        printf("6- Ogrenci sil\n");
+        printf("7- Oyuncak ekle\n");
+        printf("8- Oyuncak cikar\n");
+        printf("9- Oyuncaklari listele\n");
+        printf("0- Cikis\n");
+
+        printf("Seciminizi yapin: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                createClass(&classList);
+                break;
+            case 2:
+                addStudent(classList);
+                break;
+            case 3:
+                listClasses(classList);
+                break;
+            case 4:
+                listStudents(classList);
+                break;
+            case 5:
+                removeClass(&classList);
+                break;
+            case 6:
+                removeStudent(classList);
+                break;
+            case 7:
+                addToy(classList);
+                break;
+            case 8:
+                removeToy(classList);
+                break;
+            case 9:
+                listToys(classList);
+                break;
+            case 0:
+                printf("Program sonlandiriliyor...\n");
+                break;
+            default:
+                printf("Gecersiz secim. Tekrar deneyin.\n");
+        }
+
+    } while (choice != 0);
+
     return 0;
+}
+
+void createClass(Class** classList) {
+    Class* newClass = (Class*)malloc(sizeof(Class));
+    newClass->next = *classList;
+    *classList = newClass;
+    printf("Sinif adini girin: ");
+    scanf("%s", newClass->className);
+    newClass->studentList = NULL;
+    printf("Sinif olusturuldu: %s\n", newClass->className);
+}
+
+void addStudent(Class* classList) {
+    if (classList == NULL) {
+        printf("Ogrenci eklemek icin once sinif olusturmalisiniz.\n");
+        return;
+    }
+
+    char className[30];
+    printf("Sinif adini girin, ana menüye dönmek için 'menu' yazın: ");
+    scanf("%s", className);
+    if (strcmp(className, "menu") == 0) {
+        return;
+    }
+
+    Class* currentClass = classList;
+    while (currentClass != NULL) {
+        if (strcmp(currentClass->className, className) == 0) {
+            break;
+        }
+        currentClass = currentClass->next;
+    }
+
+    if (currentClass == NULL) {
+        printf("Boyle bir sinif bulunamadi.\n");
+        return;
+    }
+
+    Student* newStudent = (Student*)malloc(sizeof(Student));
+    printf("Ogrenci adini girin: ");
+    scanf("%s", newStudent->name);
+    printf("Ogrenci yasini girin: ");
+    scanf("%d", &newStudent->age);
+
+    newStudent->next = currentClass->studentList;
+    currentClass->studentList = newStudent;
+
+    printf("Ogrenci eklendi: %s, %d yasinda\n", newStudent->name, newStudent->age);
+}
+
+void listClasses(Class* classList) {
+    if (classList == NULL) {
+        printf("Listelenecek sinif bulunamadi.\n");
+        return;
+    }
+
+    printf("Siniflar:\n");
+    while (classList != NULL) {
+        printf("- %s\n", classList->className);
+        classList = classList->next;
+    }
+}
+
+void listStudents(Class* classList) {
+    if (classList == NULL) {
+        printf("Once sinif olusturmalisiniz.\n");
+        return;
+    }
+
+    char className[30];
+    printf("Hangi sinifin ogrencilerini listelemek istiyorsunuz? ('menu' yazarak ana menuye donun) ");
+    scanf("%s", className);
+
+    if (strcmp(className, "menu") == 0) {
+        return;
+    }
+
+    Class* currentClass = classList;
+    while (currentClass != NULL) {
+        if (strcmp(currentClass->className, className) == 0) {
+            break;
+        }
+        currentClass = currentClass->next;
+    }
+
+    if (currentClass == NULL) {
+        printf("Boyle bir sinif bulunamadi.\n");
+        return;
+    }
+
+    printf("Ogrencileri listelenen sinif: %s\n", currentClass->className);
+
+    Student* currentStudent = currentClass->studentList;
+    while (currentStudent != NULL) {
+        printf("- %s, %d yasinda\n", currentStudent->name, currentStudent->age);
+        currentStudent = currentStudent->next;
+    }
+}
+
+void removeClass(Class** classList) {
+    if (*classList == NULL) {
+        printf("Silinecek sinif bulunamadi.\n");
+        return;
+    }
+
+    printf("Silmek istediğiniz sinifin adini girin, ana menüye dönmek için 'menu' yazın: ");
+    char className[30];
+    scanf("%s", className);
+    if (strcmp(className, "menu") == 0) {
+        return;
+    }
+
+    Class* previousClass = NULL;
+    Class* currentClass = *classList;
+    while (currentClass != NULL) {
+        if (strcmp(currentClass->className, className) == 0) {
+            break;
+        }
+        previousClass = currentClass;
+        currentClass = currentClass->next;
+    }
+
+    if (currentClass == NULL) {
+        printf("Boyle bir sinif bulunamadi.\n");
+        return;
+    }
+
+    if (previousClass == NULL) {
+        *classList = currentClass->next;
+    } else {
+        previousClass->next = currentClass->next;
+    }
+
+    free(currentClass);
+    printf("Sinif silindi: %s\n", className);
+}
+
+void removeStudent(Class* classList) {
+    if (classList == NULL) {
+        printf("Once sinif olusturmalisiniz.\n");
+        return;
+    }
+
+    char className[30];
+    printf("Hangi sinifin ogrencisini silmek istiyorsunuz? ('menu' yazarak ana menuye donun) ");
+    scanf("%s", className);
+
+    if (strcmp(className, "menu") == 0) {
+        return;
+    }
+
+    Class* currentClass = classList;
+    while (currentClass != NULL) {
+        if (strcmp(currentClass->className, className) == 0) {
+            break;
+        }
+        currentClass = currentClass->next;
+    }
+
+    if (currentClass == NULL) {
+        printf("Boyle bir sinif bulunamadi.\n");
+        return;
+    }
+
+    printf("Silmek istediğiniz ogrencinin adini girin, ana menüye dönmek için 'menu' yazın: ");
+    char studentName[50];
+    scanf("%s", studentName);
+    if (strcmp(studentName, "menu") == 0) {
+        return;
+    }
+
+    Student* previousStudent = NULL;
+    Student* currentStudent = currentClass->studentList;
+    while (currentStudent != NULL) {
+        if (strcmp(currentStudent->name, studentName) == 0) {
+            break;
+        }
+        previousStudent = currentStudent;
+        currentStudent = currentStudent->next;
+    }
+
+    if (currentStudent == NULL) {
+        printf("Boyle bir ogrenci bulunamadi.\n");
+        return;
+    }
+
+    if (previousStudent == NULL) {
+        currentClass->studentList = currentStudent->next;
+    } else {
+        previousStudent->next = currentStudent->next;
+    }
+
+    free(currentStudent);
+    printf("Ogrenci silindi: %s, %s\n", studentName, className);
+}
+
+void addToy(Class* classList) {
+    char className[30];
+    printf("Oyuncak eklemek istediginiz sinifin adini girin, 'menu' yazarak ana menuye donun: ");
+    scanf("%s", className);
+
+    if (strcmp(className, "menu") == 0) {
+        return;
+    }
+
+    Class* currentClass = classList;
+    while (currentClass != NULL) {
+        if (strcmp(currentClass->className, className) == 0) {
+            break;
+        }
+        currentClass = currentClass->next;
+    }
+
+    if (currentClass == NULL) {
+        printf("Boyle bir sinif bulunamadi.\n");
+        return;
+    }
+
+    if (currentClass->toyCount == 5) {
+        printf("Bu sinifin oyuncak kapasitesi dolu.\n");
+        return;
+    }
+
+    printf("Eklemek istediginiz oyuncagi girin: ");
+    scanf("%s", currentClass->toyList[currentClass->toyCount]);
+    currentClass->toyCount++;
+
+    printf("Oyuncak eklendi: %s, %s sinifi\n", currentClass->toyList[currentClass->toyCount - 1], className);
+}
+
+void removeToy(Class* classList) {
+    char className[30];
+    printf("Oyuncak cikarmak istediginiz sinifin adini girin, 'menu' yazarak ana menuye donun: ");
+    scanf("%s", className);
+
+    if (strcmp(className, "menu") == 0) {
+        return;
+    }
+
+    Class* currentClass = classList;
+    while (currentClass != NULL) {
+        if (strcmp(currentClass->className, className) == 0) {
+            break;
+        }
+        currentClass = currentClass->next;
+    }
+
+    if (currentClass == NULL) {
+        printf("Boyle bir sinif bulunamadi.\n");
+        return;
+    }
+
+    if (currentClass->toyCount == 0) {
+        printf("Bu sinifin oyuncak listesi zaten bos.\n");
+        return;
+    }
+
+    // Oyuncak çıkarma işlemi
+    printf("Cikarmak istediginiz oyuncagi secin:\n");
+    for (int i = 0; i < currentClass->toyCount; i++) {
+        printf("%d- %s\n", i + 1, currentClass->toyList[i]);
+    }
+
+    int choice;
+    printf("Seciminizi yapin (1-%d): ", currentClass->toyCount);
+    scanf("%d", &choice);
+
+    if (choice < 1 || choice > currentClass->toyCount) {
+        printf("Gecersiz secim.\n");
+        return;
+    }
+
+    printf("Oyuncak cikarildi: %s, %s sinifi\n", currentClass->toyList[choice - 1], className);
+
+    // Seçilen oyuncak dışındaki oyuncakları kaydırma işlemi
+    for (int i = choice - 1; i < currentClass->toyCount - 1; i++) {
+        strcpy(currentClass->toyList[i], currentClass->toyList[i + 1]);
+    }
+
+    currentClass->toyCount--;
+}
+
+void listToys(Class* classList) {
+    char className[30];
+    printf("Oyuncaklari listelemek istediginiz sinifin adini girin, 'menu' yazarak ana menuye donun: ");
+    scanf("%s", className);
+
+    if (strcmp(className, "menu") == 0) {
+        return;
+    }
+
+    Class* currentClass = classList;
+    while (currentClass != NULL) {
+        if (strcmp(currentClass->className, className) == 0) {
+            break;
+        }
+        currentClass = currentClass->next;
+    }
+
+    if (currentClass == NULL) {
+        printf("Boyle bir sinif bulunamadi.\n");
+        return;
+    }
+
+    if (currentClass->toyCount == 0) {
+        printf("Bu sinifin oyuncak listesi bos.\n");
+        return;
+    }
+
+    printf("Oyuncaklar (%s sinifi):\n", currentClass->className);
+    for (int i = 0; i < currentClass->toyCount; i++) {
+        printf("- %s\n", currentClass->toyList[i]);
+    }
 }
